1fc2eae4dab917fb9082f0abdc91d6cc925a59dd
VasekZah <vaclavzahradka26@gmail.com>
2025-08-11 23:14:07 +0200

    Delete js/uiHelpers.js

--- js/uiHelpers.js
+++
@@ -1,9 +0,0 @@
-export function getUiIcon(resourceType) {
-    // Tato jednoduchá funkce vrací emoji pro daný typ suroviny v tooltipech.
-    switch (resourceType) {
-        case 'wood': return '🌲';
-        case 'stone': return '💎';
-        case 'food': return '🥩';
-        default: return '';
-    }
-}
