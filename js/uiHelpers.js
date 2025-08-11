df7f7e40b860a6549d863e0254ac2fb51f56407d
VasekZah <vaclavzahradka26@gmail.com>
2025-08-11 19:37:23 +0200

    Update uiHelpers.js

--- js/uiHelpers.js
+++ js/uiHelpers.js
@@ -1,5 +1,17 @@
-export function getUiIcon(name, classes = 'icon') {
-    // Ikony bereme ze speciální složky, abychom je oddělili od herních spritů
-    const src = `img/icons/${name}.png`;
-    return `<img src="${src}" class="${classes}" alt="${name} icon" />`;
+const uiIconCache = new Map();
+
+// Tato funkce nyní vrací data URL z cachovaného <canvas> elementu
+export function getUiIcon(spriteCache, name, classes = 'icon') {
+    if (uiIconCache.has(name)) {
+        return `<img src="${uiIconCache.get(name)}" class="${classes}" alt="${name} icon" />`;
+    }
+
+    const canvas = spriteCache.get(name);
+    if (canvas) {
+        const dataUrl = canvas.toDataURL();
+        uiIconCache.set(name, dataUrl);
+        return `<img src="${dataUrl}" class="${classes}" alt="${name} icon" />`;
+    }
+    
+    return `<span class="${classes}" style="background-color: #F87171; display: inline-block;"></span>`;
 }
