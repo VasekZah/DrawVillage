cfe9951c7cbb2ad3b7c428b62a4c0fc04954100c
VasekZah <vaclavzahradka26@gmail.com>
2025-08-11 19:08:55 +0200

    Update uiHelpers.js

--- js/uiHelpers.js
+++ js/uiHelpers.js
@@ -1,19 +1,5 @@
-import { generateSprite } from './pixel-art-data.js';
-
-const uiIconCache = new Map();
-
 export function getUiIcon(name, classes = 'icon') {
-    if (uiIconCache.has(name)) {
-        const iconSrc = uiIconCache.get(name);
-        return `<img src="${iconSrc}" class="${classes}" alt="${name} icon" />`;
-    }
-
-    const canvas = generateSprite(name);
-    if (canvas) {
-        const dataUrl = canvas.toDataURL();
-        uiIconCache.set(name, dataUrl);
-        return `<img src="${dataUrl}" class="${classes}" alt="${name} icon" />`;
-    }
-    
-    return `<span class="${classes}" style="background-color: #F87171; display: inline-block;"></span>`;
+    // Ikony bereme ze speciální složky, abychom je oddělili od herních spritů
+    const src = `img/icons/${name}.png`;
+    return `<img src="${src}" class="${classes}" alt="${name} icon" />`;
 }
