a38eef55cbebc8d79bdd6b7433b11a8f6e88a2d9
VasekZah <vaclavzahradka26@gmail.com>
2025-08-11 20:53:01 +0200

    Delete js directory

--- js/globals.js
+++
@@ -1,3 +0,0 @@
-// This object will be populated at runtime after the DOM is loaded.
-// It will hold all references to canvas, context, UI elements, and game state.
-export const G = {};
--- js/uiHelpers.js
+++
@@ -1,9 +0,0 @@
-import { G } from './globals.js';
-
-export function getAssetImg(name, classes = 'icon') {
-    const asset = G.state.loadedUserAssets[name];
-    if (asset && asset.src) {
-        return `<img src="${asset.src}" class="${classes}" alt="${name}" />`;
-    }
-    return `<span class="${classes} text-red-500 font-bold">?</span>`;
-}
